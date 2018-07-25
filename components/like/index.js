80f1970e36ba1a5f573011ba43915d37b8c562ed
wjm <1186619054@qq.com>
2018-07-25 10:47:29 +0800

    喜欢组件

--- components/like/index.js
+++ components/like/index.js
@@ -4,20 +4,35 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    // isLike: {
+    //   type: Boolean,
+    //   value: false
+    // },
+    // count: {
+    //   type: Number,
+    //   value: 99
+    // }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    like: true,
+    count: 99,
+    likeSrc: 'images/love.png',
+    disLikeSrc: 'images/dislove.png'
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    onLike(event) {
+      console.log('被点击了')
+      this.setData({
+        like: !this.data.like
+      })
+    }
   }
 })
