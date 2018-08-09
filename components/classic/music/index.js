a4bb8f1cde46e5b09d21209a345fb4c893122868
wjm <1186619054@qq.com>
2018-08-09 13:37:14 +0800

    音乐播放

--- components/classic/music/index.js
+++ components/classic/music/index.js
@@ -2,12 +2,20 @@
 import {
   classicBehavior
 } from '../classic-behavior.js'
+
+// 背景音乐播放管理对象
+const mMgr = wx.getBackgroundAudioManager()
+
 Component({
   behaviors: [classicBehavior],
   /**
    * 组件的属性列表
    */
-  properties: {},
+  properties: {
+    audioSrc: {
+      type: String
+    }
+  },
 
   /**
    * 组件的初始数据
@@ -23,6 +31,18 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    onPlay() {
+      if (this.properties.isPlay) {
+        mMgr.pause()
+        this.setData({
+          isPlay: false
+        })
+      } else {
+        mMgr.src = this.properties.audioSrc
+        this.setData({
+          isPlay: true
+        })
+      }
+    }
   }
 })
