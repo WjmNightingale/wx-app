5cecfe4e6e5dde94813ad20fd65d7110609f631c
wjm <1186619054@qq.com>
2018-09-26 11:59:27 +0800

    微信小程序授权机制

--- pages/my/my.js
+++ pages/my/my.js
@@ -1,66 +1,74 @@
-// pages/about/about.js
+// pages/my/my.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    bgImg: "../../images/my/my@bg.png",
+    aboutImg: "../../images/my/about.png"
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+
+  },
+  getUserInfo(event) {
+    // 弹窗
+    // 是否授权
+    // API
+    // button 组件 UI 让用户主动点击button
+    console.log(event.detail)
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
